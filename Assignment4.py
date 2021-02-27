efdb1efa3906a5c7a7cd2770484fc0d2574fa58a
bradlet2 <bradlet2@pdx.edu>
2021-02-26 22:20:27 -0800

    Add estimator usage from tutorial at provided URL
    demo URL:
    https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html#sphx-glr-auto-examples-cluster-plot-kmeans-digits-py

--- Assignment4.py
+++ Assignment4.py
@@ -1,5 +1,10 @@
 import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.datasets import load_digits
+from time import time
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == "__main__":
 
@@ -9,3 +14,12 @@ if __name__ == "__main__":
     n_digits = np.unique(labels).size
 
     print(f"# digits: {n_digits}; # samples: {n_samples}; # features {n_features}")
+
+    k_means = KMeans(init="k-means++", n_clusters=n_digits, n_init=4, random_state=0)
+
+    start_time = time()
+    estimator = make_pipeline(StandardScaler(), k_means).fit(data)
+    time_to_fit = time() - start_time
+
+    results = ["k-means++", time_to_fit*1000, estimator[-1].inertia_]  # multiply by 1k to get ms instead of seconds
+    print(results)
