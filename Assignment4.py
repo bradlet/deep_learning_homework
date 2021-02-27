976e406df845b29dcc0d32cee66eafc5b16ded75
bradlet2 <bradlet2@pdx.edu>
2021-02-26 22:08:30 -0800

    Use sklearn instead of pytorch

--- Assignment4.py
+++ Assignment4.py
@@ -1,30 +1,11 @@
-import torch
-import torchvision
-
-# Constants
-BATCH_SIZE = 30
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Transforms
-transform = torchvision.transforms.Compose(
-    [torchvision.transforms.ToTensor(),
-     torchvision.transforms.Normalize((0.5,), (0.5,))]
-)
-
-# Datasets
-trainset = torchvision.datasets.FashionMNIST('./data', download=True, train=True, transform=transform)
-testset = torchvision.datasets.FashionMNIST('./data', download=True, train=False, transform=transform)
-
-# Data Loaders
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+import numpy as np
+from sklearn.datasets import load_digits
 
 if __name__ == "__main__":
 
-    data, labels = next(iter(trainloader))
-    data, labels = data.to(device), labels.to(device)
+    data, labels = load_digits(return_X_y=True)
 
-    print("Data Size: ", data.shape)
-    print("Label Size: ", labels.shape)
+    n_samples, n_features = data.shape
+    n_digits = np.unique(labels).size
+
+    print(f"# digits: {n_digits}; # samples: {n_samples}; # features {n_features}")
