dc09d50547ce7e93012111d316bd261596fea049
bradlet2 <bradlet2@pdx.edu>
2021-03-10 08:25:21 -0800

    Update for new addition of celeba data being ignored

--- Assignment5.py
+++ Assignment5.py
@@ -1,6 +1,8 @@
+# Note, data for this program should live in /data/celeba
+# I have that in gitignore b/c it's too large to hold in github. This program won't work unless you download
+# The needed dataset and point data root to it.
+
 from time import time
-
-
 
 if __name__ == "__main__":
     print("Start GAN")
